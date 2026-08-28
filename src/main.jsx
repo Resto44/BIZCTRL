449cbcac0fdebc2856a444e0316a0c3133b1005c
Resto44 <haratillc@gmail.com>
2026-08-28 12:33:19 +0300

    fix: load responsive guardrails from app entry

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App.jsx';
 import '@/index.css';
+import '@/styles/responsive-performance.css';
 
 class GlobalErrorBoundary extends React.Component {
   constructor(props) {
